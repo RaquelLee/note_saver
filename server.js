a80bdaefeaf069d0c4e33c4dfdd3fd9481140fb4
rsco <RaquelLee@users.noreply.github.com>
2021-03-06 16:56:33 -0500

    api and server routes working

--- server.js
+++ server.js
@@ -1,43 +1,37 @@
-const fs = require('fs');
-const express = require('express')
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000; //heroku
 
-const app = express()
-const port = 3000
-
-// Set up body parsing, status, and route middleware
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-//http://expressjs.com/en/starter/static-files.html
+app.use(express.json());
 app.use(express.static('public'));
 
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+app.listen(PORT, () => {
+    console.log(`Listening at http://localhost:${PORT}`)
 })
 
-// The following HTML routes should be created:
+const PATH = require('path')
 
-// * `GET /notes` should return the `notes.html` file.
-app.get('', (req, res) => {
-    res.send('')
-})
+app.get('/notes', (req, res) => {
+    res.sendFile(PATH.join(__dirname, './public/notes.html'))
+});
 
-// * `GET *` should return the `index.html` file.
-app.get('', (req, res) => {
-    res.send('')
-})
+app.get('/', (req, res) => {
+    res.sendFile(PATH.join(__dirname, "./public/index.html"))
+});
 
-// The following API routes should be created:
+const fs = require('fs');
+const data = require('./db/db.json');
+const {v4 : uuidv4} = require('uuid');
 
-// * `GET /api/notes` should read the `db.json` file and return all saved notes as JSON.
-app.get('', (req, res) => {
-    res.send('')
-})
-// * `POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into `npm` packages that could do this for you).
-app.get('', (req, res) => {
-    res.send('')
-})
+app.post('/api/notes', (req,res) => {
+    req.body.id = uuidv4();
+    data.push(req.body);
+    fs.writeFile('./db/db.json', JSON.stringify(data), "utf-8", (err) => {  
+        err ? console.error(err) : console.log("Success!");
+        res.json(req.body);
+})});
 
-app.delete('/user', function (req, res) {
-    res.send('Got a DELETE request at /user')
-})
+app.get('/api/notes', (req, res) => {
+    res.json(data);
+});
